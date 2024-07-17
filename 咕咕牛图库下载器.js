920a518485d23c86373d51c60f9514a9e76a4a11
GuGuNiu <zzhjcy56@live.com>
2024-07-17 12:10:56 +0800

    预载 星铁2.5角色

--- 咕咕牛图库下载器.js
+++ 咕咕牛图库下载器.js
@@ -143,6 +143,7 @@ export class MiaoPluginMBT extends plugin {
             const sourceFile = path.join(this.localPath, '咕咕牛图库下载器.js');
             const destFile = path.join(this.JsPath, '咕咕牛图库下载器.js'); 
             await fs.promises.copyFile(sourceFile, destFile);
+            await e.reply(`『咕咕牛』图库默认将每隔15天-30天自动更新`);
         } catch (error) {
             console.error('下载『咕咕牛🐂』时出现错误:', error);
             let DowloadeErrorForward =[]
